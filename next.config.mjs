4b1b231ab2fa581d35f9b1a75af01011817fd41e
kglass-studio <glass.kathyk@gmail.com>
2025-05-28 20:54:30 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+// }
 
-export default nextConfig
+// export default nextConfig
+
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        'react-dom/server': require.resolve('react-dom/server'),
+      };
+    }
+    return config;
+  },
+  // Add this:
+  clientComponents: ['./app/(app)/clients/new/page.tsx'],
+};
+
+export default nextConfig;
