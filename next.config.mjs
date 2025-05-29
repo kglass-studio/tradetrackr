9ef7421b7f6553680cbe71f27374b9dac9b3818a
kglass-studio <glass.kathyk@gmail.com>
2025-05-28 21:36:37 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // No experimental block
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
         ...config.resolve.fallback,
         'react-dom/server': import.meta.resolve('react-dom/server'),
       };
-      config.externals = [...(config.externals || []), '@supabase/supabase-js'];
     }
     return config;
   },
