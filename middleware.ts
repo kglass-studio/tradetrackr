1b16d46494049c1853fd421d555e2f562d2eb166
kglass-studio <glass.kathyk@gmail.com>
2025-06-06 16:19:04 -0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -28,5 +28,6 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|screenshot.png).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.(png|jpg|jpeg|svg|gif)).*)"],
 };
+
